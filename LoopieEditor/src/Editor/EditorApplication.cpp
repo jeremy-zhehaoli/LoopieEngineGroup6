98c16d3872d8af9b3ae05a6edae97263aead215f
Víctor Cárdenas <victor.cardenas@estudiantat.upc.edu>
2026-01-24 14:15:02 +0100

    Update Error

--- LoopieEditor/src/Editor/EditorApplication.cpp
+++ LoopieEditor/src/Editor/EditorApplication.cpp
@@ -1,7 +1,7 @@
 #include "Editor/Modules/ProjectSetupModule.h"
 
 #include "Loopie/Core/Application.h"
-#include "Loopie/Audio/AudioManager.h"
+
 
 namespace Loopie {
 	class EditorApplication : public Application {
@@ -10,8 +10,6 @@ namespace Loopie {
 			//// Open first the module -> Project CreationSetUp
 			//// Once the Porject is selected, remove the module and add the EditorModule
 			AddModule(new ProjectSetupModule());
-
-			AddModule(new AudioManager());
 			GetWindow().SetLogo("assets/logo/logo.bmp");
 		}
 	};
